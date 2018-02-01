9c6896001e247e968bb7de20fe9533bc1315c4b5
Léa R <lea.rosamond@gmail.com>
2018-02-01 14:21:59 +0000

    first commit

---
+++ game.js
@@ -0,0 +1,19 @@
+function Morpion() {
+this.board = new Board();
+this.playerOne = new Player();
+this.playerTwo = new Player();
+this.currentMove = this.playerOne;
+}
+
+Morpion.prototype.makingMove = function () {
+
+};
+Morpion.prototype.winner = function () {
+
+};
+Morpion.prototype.reset = function () {
+
+};
+Morpion.prototype.changeTurn = function () {
+
+};
